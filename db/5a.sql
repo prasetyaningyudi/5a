e55dcf71c179399fa327be3d397ec86361ec14b0
Yudi <prasetyaningyudi@gmail.com>
2018-08-08 10:28:33 +0700

    Update db

--- db/5a.sql
+++ db/5a.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 18, 2018 at 01:32 PM
+-- Generation Time: Aug 08, 2018 at 05:28 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -41,6 +41,28 @@ CREATE TABLE `file` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `lokasi_tes`
+--
+
+CREATE TABLE `lokasi_tes` (
+  `ID` int(11) NOT NULL,
+  `NAMA_LOKASI` text NOT NULL,
+  `STATUS_LOKASI` varchar(1) NOT NULL DEFAULT '1',
+  `CREATE_DATE` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lokasi_tes`
+--
+
+INSERT INTO `lokasi_tes` (`ID`, `NAMA_LOKASI`, `STATUS_LOKASI`, `CREATE_DATE`) VALUES
+(1, 'Tangerang', '0', '2018-08-08 09:14:03'),
+(2, 'Jakarta', '1', '2018-08-08 09:14:26'),
+(3, 'Surabaya', '1', '2018-08-08 09:14:34');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `lowongan`
 --
 
@@ -50,6 +72,15 @@ CREATE TABLE `lowongan` (
   `STATUS_POSISI` varchar(1) NOT NULL DEFAULT '1',
   `CREATE_DATE` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lowongan`
+--
+
+INSERT INTO `lowongan` (`ID`, `POSISI_LOWONGAN`, `STATUS_POSISI`, `CREATE_DATE`) VALUES
+(1, 'HRD', '0', '2018-08-08 09:32:38'),
+(2, 'Sekretaris', '1', '2018-08-08 09:32:48'),
+(3, 'Direktur', '1', '2018-08-08 09:32:54');
 
 -- --------------------------------------------------------
 
@@ -79,6 +110,7 @@ CREATE TABLE `pelamar` (
   `ID` int(11) NOT NULL,
   `NAMA` text NOT NULL,
   `TELEPON` varchar(255) NOT NULL,
+  `EMAIL` text NOT NULL,
   `ALAMAT` text NOT NULL,
   `JENIS_KELAMIN` varchar(50) NOT NULL,
   `TEMPAT_LAHIR` text NOT NULL,
@@ -91,8 +123,17 @@ CREATE TABLE `pelamar` (
   `LINK_CV` text NOT NULL,
   `LINK_PHOTO` text NOT NULL,
   `CREATE_DATE` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `LOWONGAN_ID` int(11) NOT NULL
+  `LOWONGAN_ID` int(11) NOT NULL,
+  `LOKASI_TES_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pelamar`
+--
+
+INSERT INTO `pelamar` (`ID`, `NAMA`, `TELEPON`, `EMAIL`, `ALAMAT`, `JENIS_KELAMIN`, `TEMPAT_LAHIR`, `TANGGAL_LAHIR`, `STATUS_PERNIKAHAN`, `CEK_1`, `CEK_2`, `CEK_3`, `CEK_4`, `LINK_CV`, `LINK_PHOTO`, `CREATE_DATE`, `LOWONGAN_ID`, `LOKASI_TES_ID`) VALUES
+(2, 'Yudi Prasetyo', '081329680342', 'prasetyaningyudi@gmail.com', 'klaten', 'Pria', 'Klaten', '1984-10-26', 'Kawin', 'on', 'off', 'off', 'on', 'http://localhost/5a/assets/files/2018-08-08-09-43-43am_Pidi Baiq - Dilan 1991 (Bagian #2).pdf', 'http://localhost/5a/assets/images/2018-08-08-09-43-43am_1-avatar.png', '2018-08-08 09:43:43', 2, 3),
+(3, 'IMAS MASKANAH', '085762443544', 'badog.kingers@gmail.com', 'dddd', 'Wanita', 'klaten', '2018-08-13', 'Belum Kawin', 'on', 'on', 'on', 'on', 'http://localhost/5a/assets/files/2018-08-08-10-18-08am_Data Produksi.pdf', 'http://localhost/5a/assets/images/2018-08-08-10-18-08am_hand.png', '2018-08-08 10:18:08', 3, 3);
 
 -- --------------------------------------------------------
 
@@ -103,11 +144,22 @@ CREATE TABLE `pelamar` (
 CREATE TABLE `pendidikan` (
   `ID` int(11) NOT NULL,
   `TINGKAT` text NOT NULL,
+  `UNIVERSITAS` text NOT NULL,
+  `FAKULTAS` text NOT NULL,
   `JURUSAN` text NOT NULL,
+  `IPK` text NOT NULL,
   `AWAL_PENDIDIKAN` date NOT NULL,
   `AKHIR_PENDIDIKAN` date NOT NULL,
   `PELAMAR_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pendidikan`
+--
+
+INSERT INTO `pendidikan` (`ID`, `TINGKAT`, `UNIVERSITAS`, `FAKULTAS`, `JURUSAN`, `IPK`, `AWAL_PENDIDIKAN`, `AKHIR_PENDIDIKAN`, `PELAMAR_ID`) VALUES
+(1, 'S1', 'ITS', 'SI', 'Sistem Informasi', '3.56', '2018-08-05', '2018-08-17', 2),
+(2, 'S1', 'ITS', 'SI', 'Sistem Informasi', '3.56', '2018-08-06', '2018-08-22', 3);
 
 -- --------------------------------------------------------
 
@@ -123,6 +175,14 @@ CREATE TABLE `pengalaman_kerja` (
   `AKHIR_KERJA` date DEFAULT NULL,
   `PELAMAR_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengalaman_kerja`
+--
+
+INSERT INTO `pengalaman_kerja` (`ID`, `NAMA_PERUSAHAAN`, `POSISI`, `AWAL_KERJA`, `AKHIR_KERJA`, `PELAMAR_ID`) VALUES
+(1, 'PT A', 'sekre', '0000-00-00', '0000-00-00', 2),
+(2, 'PT ABC', 'DIREKTUR', '2018-08-07', '2018-08-29', 3);
 
 -- --------------------------------------------------------
 
@@ -228,6 +288,12 @@ ALTER TABLE `file`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `lokasi_tes`
+--
+ALTER TABLE `lokasi_tes`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `lowongan`
 --
 ALTER TABLE `lowongan`
@@ -245,7 +311,8 @@ ALTER TABLE `menu`
 --
 ALTER TABLE `pelamar`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_PELAMAR_LOWONGAN1_idx` (`LOWONGAN_ID`);
+  ADD KEY `fk_PELAMAR_LOWONGAN1_idx` (`LOWONGAN_ID`),
+  ADD KEY `fk_PELAMAR_LOKASI_TES1_idx` (`LOKASI_TES_ID`);
 
 --
 -- Indexes for table `pendidikan`
@@ -315,10 +382,16 @@ ALTER TABLE `file`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `lokasi_tes`
+--
+ALTER TABLE `lokasi_tes`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `lowongan`
 --
 ALTER TABLE `lowongan`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `menu`
@@ -330,19 +403,19 @@ ALTER TABLE `menu`
 -- AUTO_INCREMENT for table `pelamar`
 --
 ALTER TABLE `pelamar`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pendidikan`
 --
 ALTER TABLE `pendidikan`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pengalaman_kerja`
 --
 ALTER TABLE `pengalaman_kerja`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `ref_type`
@@ -394,6 +467,7 @@ ALTER TABLE `menu`
 -- Constraints for table `pelamar`
 --
 ALTER TABLE `pelamar`
+  ADD CONSTRAINT `fk_PELAMAR_LOKASI_TES1` FOREIGN KEY (`LOKASI_TES_ID`) REFERENCES `lokasi_tes` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_PELAMAR_LOWONGAN1` FOREIGN KEY (`LOWONGAN_ID`) REFERENCES `lowongan` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
